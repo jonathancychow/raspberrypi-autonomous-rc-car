c1c303dc69bfea86a7562554fa4954435ca30a23
Jonathan Chow <jonathan@cychow.co.uk>
2019-05-11 22:29:44 +0100

    Updated shell script and included mjpg zip

--- install_mjpeg_streamer_1.sh
+++ install_mjpeg_streamer_1.sh
@@ -2,13 +2,6 @@ sudo apt-get install libjpeg8-dev imagemagick libv4l-dev
 
 sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
 
-# Download mjpg streamer and copy to the current directory 
-repo_dir=$(pwd)
-wget "https://sourceforge.net/code-snapshots/svn/m/mj/mjpg-streamer/code/mjpg-streamer-code-r182.zip"
-cd /home/pi/Downloads
-cp mjpg-streamer-code-r182.zip $repo_dir
-cd $repo_dir
-
 # Unzip and make
 mkdir mjpg
 unzip mjpg-streamer-code-r182.zip -d mjpg
