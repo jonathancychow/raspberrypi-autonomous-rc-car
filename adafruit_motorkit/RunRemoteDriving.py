bacb973c2d83d0fd22a631ba14b1e0d57471eb5b
Jonathan Chow <jonathan@cychow.co.uk>
2019-06-08 22:41:30 +0100

    Added TurningRight and TuringLeft

--- adafruit_motorkit/RunRemoteDriving.py
+++ adafruit_motorkit/RunRemoteDriving.py
@@ -5,13 +5,25 @@ from adafruit_motorkit import MotorKit
 
 kit = MotorKit()
 
-# Turn both motors forwards
+# Calibration
+GxRightMotorForward = 1.0
+GxLeftMotorForward = -1.0
+
+# Switch both motors on - go forwards
 def Forwards():
-    kit.motor1.throttle = 1.0
-    kit.motor2.throttle = -1.0
-# Turn both motors backwards
+    kit.motor1.throttle = GxRightMotorForward
+    kit.motor2.throttle = GxLeftMotorForward
+# Switch both motors off - stop
 def Stop():
     kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
+# Switch the right motor on - go left
+def TuringLeft():
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = GxRightMotorForward
+# Switch the left motor off = go right
+def TurningRight():
+    kit.motor1.throttle = GxLeftMotorForward
     kit.motor2.throttle = 0
 
 # Your code to control the robot goes below this line
@@ -23,9 +35,15 @@ try:
         if key_input == 'w':
           Forwards()
           print('Going Forward')
-        if key_input == 'd':
+        if key_input == 's':
           Stop()
           print('Going Right')
+        if key_input == 'd':
+          TurningRight()
+          print('Going Right')
+        if key_input == 'a':
+          TurningRight()
+          print('Going Left')
 # If you press CTRL+C, cleanup and stop
 except KeyboardInterrupt:
     print('Exit')
