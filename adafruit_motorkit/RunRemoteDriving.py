290026f4c63fba799718e4e414187bf6e15d0cf2
Jonathan Chow <jonathan@cychow.co.uk>
2019-06-09 16:03:30 +0100

    Added going backward functionality

--- adafruit_motorkit/RunRemoteDriving.py
+++ adafruit_motorkit/RunRemoteDriving.py
@@ -9,18 +9,26 @@ kit = MotorKit()
 GxRightMotorForward = 1.0
 GxLeftMotorForward = 1.0
 
+# Switch both motors on - go backwards
+def Backwards():
+    kit.motor1.throttle = -GxRightMotorForward
+    kit.motor2.throttle = -GxLeftMotorForward
+
 # Switch both motors on - go forwards
 def Forwards():
     kit.motor1.throttle = GxRightMotorForward
     kit.motor2.throttle = GxLeftMotorForward
+
 # Switch both motors off - stop
 def Stop():
     kit.motor1.throttle = 0
     kit.motor2.throttle = 0
+
 # Switch the right motor on - go left
 def TurningLeft():
     kit.motor1.throttle = GxLeftMotorForward
     kit.motor2.throttle = -GxRightMotorForward
+
 # Switch the left motor off = go right
 def TurningRight():
     kit.motor1.throttle = -GxLeftMotorForward
@@ -35,15 +43,19 @@ try:
         if key_input == 'w':
           Forwards()
           print('Going Forward')
-        if key_input == 's':
+        if key_input == 'e':
           Stop()
-          print('Going Right')
+          print('Stop')
         if key_input == 'd':
           TurningRight()
           print('Going Right')
         if key_input == 'a':
           TurningLeft()
           print('Going Left')
+        if key_input == 's':
+          Backwards()
+          print('Going Backward')
+
 # If you press CTRL+C, cleanup and stop
 except KeyboardInterrupt:
     print('Exit')
